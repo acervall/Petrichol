edebe459f271424ffa8f6a0b3e526470c678e6c0
Amelie Cervall <amelie@grebban.com>
2023-11-13 16:12:04 +0100

    react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,50 @@
-import {  Routes, Route } from 'react-router-dom'
 import Home from './views/Home'
 import UserProfile from './views/UserProfile'
 import Lists from './views/Lists'
 
+import {
+  createHashRouter,
+  Link,
+  Outlet,
+  RouterProvider
+} from 'react-router-dom'
 
-
-function App() {
-
+function Root() {
   return (
     <>
-       <Routes>
-                <Route path="/" Component={Home} />
-                <Route path="/profile" Component={UserProfile}/>
-                <Route path="/lists" Component={Lists}/>
-        </Routes>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/lists">Lists</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
     </>
   )
+}
+
+function App() {
+  const router = createHashRouter([
+    {
+      children: [
+        { element: <Home />, path: '/' },
+        { element: <UserProfile />, path: '/profile' },
+        { element: <Lists />, path: '/lists' }
+      ],
+      element: <Root />
+    }
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App
