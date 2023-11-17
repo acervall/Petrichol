b6236b1169e825f393a361d1da3e6cdf57b3caa2
Hanne-Kristin-Steensath <hanne.kristin.steensath@gmail.com>
2023-11-17 11:31:01 +0100

    add lazy to App.tsx, code splitting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,69 @@
-import Home from './views/Home'
+import { lazy, Suspense } from 'react'
+
+const Home = lazy(() => import('./views/Home'))
+const UserProfile = lazy(() => import('./views/UserProfile'))
+const Lists = lazy(() => import('./views/Lists'))
+const ListDetail = lazy(() => import('./components/ListDetails'))
+
+import { createHashRouter, Outlet, RouterProvider } from 'react-router-dom'
+
+function Root() {
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
+
+function App() {
+  const router = createHashRouter([
+    {
+      children: [
+        {
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          ),
+          path: '/',
+        },
+        {
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <UserProfile />
+            </Suspense>
+          ),
+          path: '/profile',
+        },
+        {
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Lists />
+            </Suspense>
+          ),
+          path: '/lists',
+        },
+        {
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <ListDetail />
+            </Suspense>
+          ),
+          path: '/lists/:listId',
+        },
+      ],
+      element: <Root />,
+    },
+  ])
+
+  return <RouterProvider router={router} />
+}
+
+export default App
+
+/*import Home from './views/Home'
 import UserProfile from './views/UserProfile'
 import Lists from './views/Lists'
 import ListDetail from './components/ListDetails'
@@ -32,7 +97,7 @@ function App() {
   return <RouterProvider router={router} />
 }
 
-export default App
+export default App*/
 
 /*import Home from './views/Home'
 import UserProfile from './views/UserProfile'
