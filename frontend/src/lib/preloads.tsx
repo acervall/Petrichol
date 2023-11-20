68539da6d06fe51c16bc53b449417dc97844c92c
Hanne-Kristin-Steensath <hanne.kristin.steensath@gmail.com>
2023-11-20 13:15:28 +0100

    preloades.tsx

--- frontend/src/lib/preloads.tsx
+++ frontend/src/lib/preloads.tsx
@@ -1,6 +1,11 @@
-import { lazyWithPreload } from 'react-lazy-with-preload';
+import { lazyWithPreload } from 'react-lazy-with-preload'
 
-export const Home = lazyWithPreload(() => import('../views/Home'));
-export const Lists = lazyWithPreload(() => import('../views/Lists'));
-export const UserProfile = lazyWithPreload(() => import('../views/UserProfile'));
-export const ListDetails = lazyWithPreload(() => import('../components/ListDetails'));
+export const Home = lazyWithPreload(() => import('../views/Home'))
+export const Lists = lazyWithPreload(() => import('../views/Lists'))
+export const UserProfile = lazyWithPreload(() => import('../views/UserProfile'))
+export const ListDetails = lazyWithPreload(
+  () => import('../components/ListDetails'),
+)
+export const FolderDetails = lazyWithPreload(
+  () => import('../components/FolderDetails'),
+)
