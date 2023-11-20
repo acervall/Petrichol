d3188169169dbd9ca303ae4adcacfa43684beb3a
Amelie Cervall <amelie@grebban.com>
2023-11-20 13:35:13 +0100

    update home

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,30 +1,57 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SigninSignup from '../components/SigninSignup'
 import List from '../components/List'
-
+import Navbar from '../components/Navbar'
+import { useLogoutUser, useUserData } from '../store/userStore'
+import Folder from '../components/Folder'
 
 function Home() {
-  const [loggedIn, setLoggedIn] = useState(true)
-  // TODO: add login status, context?
+  const { data: user, isLoading, isSuccess } = useUserData()
+  const logoutUser = useLogoutUser()
+  const [loggedIn, setLoggedIn] = useState(false)
 
-  if (loggedIn) {
-    return (
-      <>
-        <div>Home</div>
-        <button onClick={() => setLoggedIn(false)}>
-          Set logged in to false
-        </button>
-        <List />
-      </>
-    )
-  } else {
-    return (
-      <>
-        <SigninSignup />
-        <button onClick={() => setLoggedIn(true)}>Set logged in to true</button>
-      </>
-    )
+  useEffect(() => {
+    console.log('change login')
+    // console.log('loggedIn', loggedIn)
+    // console.log('user', user)
+    console.log('isSuccess', isSuccess)
+
+    if (isSuccess) {
+      setLoggedIn(true)
+    }
+  }, [isSuccess])
+
+  const handleLogout = async () => {
+    await logoutUser.mutateAsync()
+    setLoggedIn(false)
   }
+
+  const handleClick = () => {
+    console.log('loggedIn', loggedIn)
+    console.log('user', user)
+    console.log('isSuccess', isSuccess)
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <>
+      {loggedIn ? (
+        <>
+          <div>Home</div>
+          <button onClick={handleLogout}>Log out</button>
+          <button onClick={handleClick}>KLICK</button>
+          <Navbar />
+          <Folder />
+          <List />
+        </>
+      ) : (
+        <SigninSignup onLogin={() => setLoggedIn(true)} />
+      )}
+    </>
+  )
 }
 
 export default Home
